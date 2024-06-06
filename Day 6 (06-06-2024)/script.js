613077c84307036fec94fabc8e1a77a14dfc6f93
Lycheeteacode <lycheteacode13@gmail.com>
2024-06-06 19:34:43 +0800

    Typeof and Variable

---
+++ Day 6 (06-06-2024)/script.js
@@ -0,0 +1,18 @@
+// *TypeOf
+// typeof adalah untuk mengecek type dari tipe data entah number, string or boolean.
+
+// *Variable
+// Penulisan Variable:
+// var nama;
+// var umur;
+// var lulus;
+
+// nama = "Lychee";
+// umur = 23;
+// lulus = true;
+
+// or
+
+// var nama = "Lychee",
+//     umur = 23,
+//     lulus = true;
