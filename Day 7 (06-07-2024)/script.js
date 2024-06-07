3b769dde04aea64baa5ee03a3e821e2ae20d0938
Lycheeteacode <lycheteacode13@gmail.com>
2024-06-07 19:01:23 +0800

    Object Construction

---
+++ Day 7 (06-07-2024)/script.js
@@ -0,0 +1,9 @@
+// Membuat Object
+// Object Construction
+// function Mahasiswa(nama, jurusan, asal) {
+//     this.name = nama;
+//     this.faculty = jurusan;
+//     this.country = asal;
+// }
+
+// let Mhs = new Mahasiswa("Lee", "Computes Science", "Indonesia");
