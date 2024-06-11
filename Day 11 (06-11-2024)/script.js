a9ac8258cfbd4c13ac6412c8450c762d02ddd8b0
Lycheeteacode <lycheteacode13@gmail.com>
2024-06-11 20:18:27 +0800

    Dom Tree and Dom Selection

---
+++ Day 11 (06-11-2024)/script.js
@@ -0,0 +1,34 @@
+// Dom
+
+// Dom Tree
+// nodeList dan HTMLCollection adalah kumpulan node.
+// nodeList itu apapun sedangkan HTMLCollection hanya elemen HTML.
+// Parent node and Child node
+
+// Dom Selection
+// getElementById()  ELEMENT
+// getElementsByTagName()   HTMLCOLLECTION
+// getElementsByClassName()  HTMLCOLLECTION
+// querySelector()  ELEMENT
+// querySelectorAll()  NODELIST
+
+// const container = document.querySelector(".container");
+// const helloBro = container.querySelector('h1');
+// const yooMan = container.querySelector('h2');
+
+// helloBro.style.color = 'red';
+// yooMan.style.color = 'aqua';
+// container.style.backgroundColor = '#b2b2b2';
+
+
+// const container = document.querySelector('.container');
+// const h1 = container.getElementsByTagName('h1');
+// const h2 = container.getElementsByTagName('h2');
+
+// h1[0].style.color = 'aqua';
+// h1[1].style.color = 'red';
+// h1[2].style.color = 'yellow';
+// h2[0].style.color = 'lightblue';
+// h2[1].style.color = 'pink';
+// h2[2].style.color = 'orange';
+
