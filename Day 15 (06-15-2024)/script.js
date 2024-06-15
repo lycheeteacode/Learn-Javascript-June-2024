604a7061cbdc3579de95156cb4aaec796380a6f1
Lycheeteacode <lycheteacode13@gmail.com>
2024-06-15 15:59:44 +0800

    Dom Manipulation

---
+++ Day 15 (06-15-2024)/script.js
@@ -0,0 +1,14 @@
+// Dom Manipulation
+
+// Manipulation Element
+// const titleWeb = document.getElementById('ttl_web');
+// const container = document.querySelector('.container');
+// const sctA = container.querySelector('#a');
+// const sctB = container.querySelector('#b');
+// const btn = document.querySelector('.btn');
+// sctA.style.backgroundColor = 'limegreen';
+// btn.style.backgroundColor = 'aqua';
+// document.addEventListener('click', e => console.log(e));
+
+
+// Manipulation Node
