ae4ca578494821e76818a22cdefe6f99e8257a1c
Lycheeteacode <lycheteacode13@gmail.com>
2024-06-21 23:24:26 +0800

    Learn Event

---
+++ Day 21 (06-21-2024)/script.js
@@ -0,0 +1,11 @@
+// Event
+
+// const helloWorld = document.querySelector('p')
+// helloWorld.addEventListener('click', () => {
+//     helloWorld.style.backgroundColor = 'lightblue';
+//     helloWorld.style.color = 'yellow';
+//     helloWorld.style.fontSize = '20px';
+//     helloWorld.style.width = '125px';
+//     helloWorld.style.height = '75px';
+//     helloWorld.style.transition = '0.2s ease';
+// })
